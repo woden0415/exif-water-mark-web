6621283139decf97a3593e2abadf2f5c04989aca
王冬 <wangdong7871@100.me>
2023-11-11 08:52:50 +0800

    feat: [添加版本校验]

---
+++ version-check.mjs
@@ -0,0 +1,13 @@
+import semver from 'semver'; 
+import { exit } from 'node:process'; 
+import chalk from 'chalk'; 
+import {readFileSync} from "fs"; 
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf8')); 
+const nodeVersionRequired = packageJson.nodeVersion.lower 
+const nodeCurrent = process.versions.node; 
+if (semver.gt(nodeCurrent, nodeVersionRequired)) { 
+  console.log(`当前node版本为${chalk.green(nodeCurrent)}，符合版本要求${chalk.red(nodeVersionRequired)}`); 
+} else { 
+  console.error(`当前node版本为${chalk.red(nodeCurrent)}, 不满足最低版本${chalk.red(nodeVersionRequired)}要求，请升级node版本`); 
+  exit(1); 
+} 
