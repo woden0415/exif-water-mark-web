2148cf578c8a3fe1b227b9df24ad6318b83496f0
王冬 <wangdong7871@100.me>
2023-11-13 11:40:44 +0800

    feat: [版本校验]

--- version-check.mjs
+++ version-check.mjs
@@ -6,7 +6,7 @@ const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
 const nodeVersionRequired = packageJson.nodeVersion.lower 
 const nodeCurrent = process.versions.node; 
 if (semver.gt(nodeCurrent, nodeVersionRequired)) { 
-  console.log(`当前node版本为${chalk.green(nodeCurrent)}，符合版本要求${chalk.red(nodeVersionRequired)}`); 
+  console.log(`当前node版本为${chalk.green(nodeCurrent)}，符合版本要求${chalk.green(nodeVersionRequired)}`); 
 } else { 
   console.error(`当前node版本为${chalk.red(nodeCurrent)}, 不满足最低版本${chalk.red(nodeVersionRequired)}要求，请升级node版本`); 
   exit(1); 
