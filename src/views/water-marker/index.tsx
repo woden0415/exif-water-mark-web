1d42e741adfa244c216afcb9f3976d00d6c76141
王冬 <wangdong7871@100.me>
2023-12-15 13:56:06 +0800

    feat: [优化展示]

--- src/views/water-marker/index.tsx
+++ src/views/water-marker/index.tsx
@@ -7,7 +7,8 @@ import {
   Page,
 } from 'konsta/react'
 import { ChangeEvent, useRef, useState } from 'react'
-import { getImageInfo } from '../../utils/index'
+import { isEmpty } from '../../utils'
+import { getImageInfo } from '../../utils/utilExif'
 
 const typeExifInfo = {
   Make: '',
@@ -32,18 +33,21 @@ export default function WaterMarker() {
     reader1.onload = (e: ProgressEvent<FileReader>) => {
       const imageArrayBuffer = e!.target!.result
       const _exifInfo = getImageInfo(imageArrayBuffer as ArrayBuffer)
-      setExifInfo({
-        Make: _exifInfo.Make,
-        Model: _exifInfo.Model,
-        LensModel: _exifInfo.LensModel,
-        FocalLengthDesc: _exifInfo.FocalLength.numerator + 'mm',
-        FNumberDesc: `${
-          _exifInfo.FNumber.numerator / _exifInfo.FNumber.denominator
-        }`,
-        ExposureTimeDesc: `${_exifInfo.ExposureTime.numerator}/${_exifInfo.ExposureTime.denominator}秒`,
-        ISOSpeedRatings: `${_exifInfo.ISOSpeedRatings}`,
-        DateTime: `${_exifInfo.DateTime}`,
-      })
+      console.log('_exifInfo :>> ', _exifInfo)
+      if (_exifInfo) {
+        setExifInfo({
+          Make: _exifInfo.Make,
+          Model: _exifInfo.Model,
+          LensModel: _exifInfo.LensModel,
+          FocalLengthDesc: _exifInfo.FocalLength?.numerator + 'mm',
+          FNumberDesc: `${
+            _exifInfo.FNumber?.numerator / _exifInfo.FNumber?.denominator
+          }`,
+          ExposureTimeDesc: `${_exifInfo.ExposureTime?.numerator}/${_exifInfo.ExposureTime?.denominator}秒`,
+          ISOSpeedRatings: `${_exifInfo.ISOSpeedRatings}`,
+          DateTime: `${_exifInfo.DateTime}`,
+        })
+      }
     }
     reader1.readAsArrayBuffer(_selectFile!)
 
@@ -86,7 +90,7 @@ export default function WaterMarker() {
       </Block>
       <Block>{base64 ? <img src={base64} alt="导入的图片" /> : null}</Block>
       <Block>
-        {exifInfo ? (
+        {!isEmpty(exifInfo) ? (
           <>
             <ul>
               <li>
@@ -102,7 +106,7 @@ export default function WaterMarker() {
             </ul>
           </>
         ) : (
-          <>没有信息</>
+          <>没有exif信息</>
         )}
       </Block>
     </Page>
